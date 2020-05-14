006a07e022886ef4bbe58dd5c4371eec90d62242
bdavis171 <bdavis171@gmail.com>
2020-05-14 17:30:50 -0500

    created blatoise and its corresponding moves and set the appropriate values

--- pokemon-battle.cpp
+++ pokemon-battle.cpp
@@ -23,11 +23,12 @@ class Pokemon
 public:
     string name,type;
     int hp,atk,def,spAtk,spDef,spd;
-    void setValues(string, string, int, int, int, int, int, int);
+    Moves move1,move2,move3,move4;
+    void setValues(string, string, int, int, int, int, int, int,Moves,Moves,Moves,Moves);
 };
 
 // method to set values for Pokemon class
-void Pokemon::setValues(string pokeName, string pokeType, int health, int attack, int defense, int specialAttack, int specialDefense, int speed){
+void Pokemon::setValues(string pokeName, string pokeType, int health, int attack, int defense, int specialAttack, int specialDefense, int speed,Moves pokeMove1, Moves pokeMove2, Moves pokeMove3, Moves pokeMove4){
     name = pokeName;
     type = pokeType;
     hp = health;
@@ -36,20 +37,45 @@ void Pokemon::setValues(string pokeName, string pokeType, int health, int attack
     spAtk = specialAttack;
     spDef = specialDefense;
     spd = speed;
+    move1 = pokeMove1;
+    move2 = pokeMove2;
+    move3 = pokeMove3;
+    move4 = pokeMove4;
 };
 
 
 // declaration function
 int main() {
-    // create a new instance of the Pokemon class called Typholsion
-    Pokemon typholsion;
-    // create new instances of the Moves class for the following moves and set their values to the appropriate ones: Flamethrower,Wild Charge,Extrasensory, and Brick Break
+    // create a new instance of the Pokemon class called Typhlosion
+    Pokemon typhlosion;
+
+    // create a new instance of the Pokemon class called Blastoise
+    Pokemon blastoise;
+
+    // (Typhlosion) create new instances of the Moves class for the following moves and set their values to the appropriate ones: Flamethrower,Wild Charge,Extrasensory, and Brick Break
     Moves flamethrower,wildCharge,extrasensory,brickBreak;
+    // (Typhlosion) Move 1
     flamethrower.setValues("Flamethrower","Fire",90,100);
+    // (Typhlosion) Move 2
     wildCharge.setValues("Wild Charge","Electric",90,100);
+    // (Typhlosion) Move 3
     extrasensory.setValues("Extrasensory","Psychic",80,100);
+    // (Typhlosion) Move 4
     brickBreak.setValues("Brick Break","Fighting",75,100);
-    // set typholsion's values to the appropriate ones
-    typholsion.setValues("Typhlosion","Fire",322,204,192,254,231,236);
-    cout << "Name: " << typholsion.name << "\n";
+
+    // (Blastoise) create new instances of the Moves class for the following moves and set their values to the appropriate ones: Hydro Pump, Dragon Pulse, Flash Cannon, and Dark Pulse
+    Moves hydroPump,dragonPulse,flashCannon,darkPulse;
+    // (Blastoise) Move 1
+    hydroPump.setValues("Hydro Pump","Water",110,80);
+    // (Blastoise) Move 2
+    dragonPulse.setValues("Dragon Pulse","Dragon",85,100);
+    // (Blastoise) Move 3
+    flashCannon.setValues("Flash Cannon","Steel",80,100);
+    // (Blastoise) Move 4
+    darkPulse.setValues("Dark Pulse","Dark",80,100);
+
+    // set typhlosion's values to the appropriate ones
+    typhlosion.setValues("Typhlosion","Fire",322,204,192,254,231,236,flamethrower,wildCharge,extrasensory,brickBreak);
+    cout << "Name: " << typhlosion.name << "\n";
+    cout << "Move 1: " << typhlosion.move1.name << "\n";
 }
